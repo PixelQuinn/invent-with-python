6ea71d368d2559ece48f6c760a40193eca903850
PixelQuinn <quinn.reams@icloud.com>
2023-10-24 17:18:42 -0400

    feat: display results function set up

--- dragon.py
+++ dragon.py
@@ -18,3 +18,13 @@ def chooseCave():
 
     return cave 
 
+# function for set-up dialogue for cave choice
+def checkCave (chosenCave) :
+    print('You approach the cave...')
+    time.sleep(2) # sleep function for a temp pause
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print()
+    time.sleep(2)
+
