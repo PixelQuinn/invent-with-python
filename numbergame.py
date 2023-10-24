457400251cba0587a86e711edfeef574660623c7
PixelQuinn <quinn.reams@icloud.com>
2023-10-23 22:23:37 -0400

    feat: number range expanded

--- numbergame.py
+++ numbergame.py
@@ -6,7 +6,7 @@ guessesTaken = 0
 print('Hello! What is your name?')
 myName = input()
 
-number = random.randint(1, 20)
+number = random.randint(1, 100)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 for guessesTasken in range(6):
