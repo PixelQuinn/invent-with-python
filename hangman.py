e7a06e5805a2821167927977a7c8b6a1428e201e
PixelQuinn <quinn.reams@icloud.com>
2023-10-25 13:34:15 -0400

    feat: function defined to get word

--- hangman.py
+++ hangman.py
@@ -40,3 +40,9 @@ HANGMAN_PICS = ['''
 
 # Words for hangman solution
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra' .split()
+
+def getRandomWord(wordList) :
+    # This function returns a random string from the passed list of strings.
+    wordIndex = random.randint(0, len(wordList) - 1)
+    return wordList[wordIndex]
+
