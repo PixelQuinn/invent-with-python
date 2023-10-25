3961a068770252585f3734576228995015a1f5a4
PixelQuinn <quinn.reams@icloud.com>
2023-10-25 16:31:39 -0400

    feat: display board added

--- hangman.py
+++ hangman.py
@@ -46,3 +46,23 @@ def getRandomWord(wordList) :
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
+# Display board function, to show hangman, correct letters, and missed letters
+def displayBoard(missedLetters, correctLetters, secretWord) :
+    print(HANGMAN_PICS [len(missedLetters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)) : # Replace blanks with correctly guessed letters
+        if secretWord [i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: # Show the secret word with spaces in between each letters.
+        print(letter, end=' ')
+    print()
+
