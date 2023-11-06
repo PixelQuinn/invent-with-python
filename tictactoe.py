aa87a6c5d16d5b834fbea07eb71f3c1119899035
PixelQuinn <quinn.reams@icloud.com>
2023-11-06 15:33:07 -0500

    feat: func added for player to choose letter
    case insensitive choose letter func

--- tictactoe.py
+++ tictactoe.py
@@ -2,12 +2,20 @@
 
 import random
 
-def drawBoard(board) :
+def drawBoard (board) :
     # This function prints out the board that it was passed.
 
     # "board" is a list of 10 strings representing the board (ignore index 0)
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print('-+-+-')
+    print (board[7] + '|' + board[8] + '|' + board[9])
+    print ('-+-+-')
     print (board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
+    print ('-+-+-')
     print (board[1] + '|' + board [2] + '|' + board[3])
+
+def inputPlayerLetter () :
+    # Lets the player type which letter they want to be.
+    # Returns a list with the player's letter as the first item, and the computer's letter as the second.
+    letter = ''
+    while not (letter == 'X' or letter == '0') :
+        print ('Do you want to be X or 0?')
+        letter = input().upper()
