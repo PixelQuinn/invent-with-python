4e3e091262dc679d42a768d647dbb4ac72008c0b
PixelQuinn <quinn.reams@icloud.com>
2023-11-06 17:15:35 -0500

    feat: function for computer to make copy of board

--- tictactoe.py
+++ tictactoe.py
@@ -38,6 +38,7 @@ def makeMove (board, letter, move) :
 
 def isWinner (bo, le) :
     # Given a board and a player's letter, this function returns True if that player has won.
+    # I used bo and le instead of board and letter so I don't have to type as much.
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
             (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
             (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
