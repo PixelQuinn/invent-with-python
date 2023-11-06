80a6d69a41b06ea66e332d8d2128abfeeb2e329c
PixelQuinn <quinn.reams@icloud.com>
2023-11-06 16:42:37 -0500

    feat: func to decide who goes first

--- tictactoe.py
+++ tictactoe.py
@@ -25,3 +25,10 @@ if letter == 'X' :
     return ['X','O']
 else :
     return ['O', 'X']
+
+def whoGoesFirst () :
+    # Randomly choose the player who goes first.
+    if random.randint(0,1) == 0 :
+        return 'computer'
+    else :
+        return 'player'
