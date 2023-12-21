8051a68b0b85755f936850378963628c64dd261a
PixelQuinn <quinn.reams@icloud.com>
2023-12-21 13:31:30 -0500

    feat: function for clues defined

--- bagels.py
+++ bagels.py
@@ -13,3 +13,20 @@ def getSecretNum() :
         secretNum += str (numbers[i])
     return secretNum
 
+# Function for clues.
+def getClues (guess, secretNum) :
+    # Returns a string with the Pico, Fermi, & Bagels clues to the user.
+    if guess == secretNum:
+        return 'You got it! Good Job!'
+    
+    clues = []
+    for i in range (len(guess)) :
+        if guess[i] == secretNum[i] :
+            clues.append('Fermi')
+        elif guess [i] in secretNum:
+            clues.append('Pico')
+        if len(clues) == 0:
+            return 'Bagels'
+        
+        clues.sort()
+        return ' '.join(clues)
